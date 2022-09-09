858c57d89d3af43929ac90685e284dbd8b72acf0
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-08 19:07:40 -0700

    8. fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main
+ * main - entry point
  *
- * return
+ * Return - a value of 1
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[];
+	
+	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	fprintf(stderr, "%s", str);
 	return (1);
 }
