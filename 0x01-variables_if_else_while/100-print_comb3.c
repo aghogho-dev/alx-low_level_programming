28dfcf5ee515117efc0b54eaaecf9d7c407151cb
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-09 20:02:39 -0700

    10. Manipulate digit with ASCII numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,45 +7,35 @@
  */
 int main(void)
 {
-	int first;
-	int second;
-	int iter;
-	int tmp;
+	int first, second, iter, tmp;
 
 	first = 0;
 	second = 1;
-
 	while (1)
 	{
 		iter = 10 - second;
 		tmp = second;
-
 		while (iter)
 		{
 			putchar(first + '0');
 			putchar(tmp++ + '0');
-
 			if (first != 8)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-
 			iter--;
-
 			if (iter == 0)
 			{
 				first = second;
 				second += 1;
 			}
 		}
-
 		if (second == 10)
 		{
 			putchar('\n');
 			break;
 		}
 	}
-
 	return (0);
 }
