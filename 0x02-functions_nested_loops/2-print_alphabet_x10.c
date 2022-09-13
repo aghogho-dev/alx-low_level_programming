ab6b00229f752a1176517aed74ffae32c2e09f27
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-13 15:35:04 -0700

    2. 10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10x
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
