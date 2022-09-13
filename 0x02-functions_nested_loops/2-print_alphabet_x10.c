dcec7ef51a439f7ea8c5766bd8e69591daba2bef
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-13 15:53:35 -0700

    2. print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print the alphabet 10x
+ * print_alphabet_x10 - use nested while loop
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int j;
+	char c;
 
-	i = 0;
-
-	while (i <= 10)
+	j = 1;
+	while (j <= 10)
 	{
-		print_alphabet();
-		i++;
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c++);
+		}
+		_putchar('\n');
+
+		j++;
 	}
 }
