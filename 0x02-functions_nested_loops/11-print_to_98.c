b342053c648df416484dca72a52ddc537816b2bb
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-14 05:00:28 -0700

    11 98 Battery string

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,75 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print up to 98
+ * @n: starting point
+ * Return: void;
+ */
+void if_one(int n)
+{
+	if (n >= 100)
+	{
+		_putchar(((n / 10) / 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n >= 10 && n < 100)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n >= 0 && n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		_putchar('-');
+		if (n <= -10)
+		{
+			_putchar((-1 * n / 10) + '0');
+			_putchar((-1 * n % 10) + '0');
+		}
+		else
+		{
+			_putchar((-1 * n % 10) + '0');
+		}
+	}
+}
+
+void if_two(int n, int k)
+{
+	if (n != k)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
+void print_to_98(int  n)
+{
+	int k = 98;
+
+	if (n > k)
+	{
+		while (n >= k)
+		{
+			if_one(n);
+			if_two(n, k);
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= k)
+		{
+			if_one(n);
+			if_two(n, k);
+			n++;
+		}
+	}
+}
