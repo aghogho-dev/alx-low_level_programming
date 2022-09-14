59cbc7ce3498ccc85dfe3579b281bb7fc88f9f5b
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-14 01:37:27 -0700

    9. times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,25 +6,29 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, k;
 
 	i = 0;
-
 	while (i <= 9)
 	{
 		j = 0;
-
 		while (j <= 9)
 		{
-			if (j != 9)
+			k = i * j;
+			if (k < 10)
 			{
-				_putchar(i*j + '0');
-				_putchar(',');
-				_putchar(' ');
+				_putchar(k + '0');
 			}
 			else
 			{
-				_putchar(i*j + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 			j++;
 		}
