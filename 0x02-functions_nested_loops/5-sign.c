588d7a8c8b17d7874ab858a939e3d772d8d77283
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-13 18:32:06 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ * @n: number to be checked
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
