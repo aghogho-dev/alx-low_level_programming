04274f0e4628f403878bcb46623214490a2e48c3
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-16 05:05:44 -0700

    11. prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * main - find primes
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int a, b, c, n, max;
+
+	n = 612852475143;
+	max = 2;
+	c = 1;
+
+	for (a = 2; a < n; a++)
+	{
+		if (n % a == 0)
+		{
+			for (b = 2; b < a; b++)
+				if (a % b == 0)
+				{
+					c = 0;
+					break;
+				}
+			if (c == 1)
+			{
+				if (a > max)
+					max = a;
+			}
+			else
+			{
+				c = 1;
+			}
+		}
+	}
+	printf("%lu\n", max);
+	return (0);
+}
+
