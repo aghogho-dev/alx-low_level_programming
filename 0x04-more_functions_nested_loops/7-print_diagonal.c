49292f465bdbe88d46b5e5b67793a71a5255fcf8
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-15 18:17:19 -0700

    7. diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print along diag
+ * @n: param
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int b, c;
+
+	if ( n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (b = 1; b <= n; b++)
+		{
+			for (c = 1; c < b; c++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
