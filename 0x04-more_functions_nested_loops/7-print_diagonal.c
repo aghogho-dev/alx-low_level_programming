0493c7613c7b078202fed3be335951a5ab462cfc
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-15 18:30:27 -0700

    7. diag

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,22 @@
 
 void print_diagonal(int n)
 {
-	int b, c;
+int b, c;
 
-	if ( n <= 0)
+if ( n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (b = 1; b <= n; b++)
 	{
+		for (c = 1; c < b; c++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
 		_putchar('\n');
 	}
-	else
-	{
-		for (b = 1; b <= n; b++)
-		{
-			for (c = 1; c < b; c++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
+}
 }
