1567959cd4293b9cfe993389a4be674f4254a558
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-21 06:56:38 -0700

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - convert number str to int
+ * @s: param
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int sn;
+	unsigned int num;
+
+	sn = 1;
+	num = 0;
+
+	do
+	{
+		if (*s == '-')
+			sn *= 1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	}
+	while (s*++);
+
+	return (sn * num);
+}
