413186b49d6f1ae8ef6e5ae3fe45db67b07f1f3c
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-20 20:32:24 -0700

    11. password gen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: int
+ */
+
+int main(void)
+{
+	int i;
+	char ar[14];
+
+	for (i = 0; i < 13; i++)
+	{
+		ar[i] = (33 + rand() % 90);
+	}
+	ar[13] = '\0';
+	printf("%s\n", ar);
+}
