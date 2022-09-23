777c41e1879c53027933ef92cfceb5e066d39ae9
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-22 19:42:51 -0700

    7. Mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,23 +9,20 @@
 char *leet(char *s)
 {
 	int i, j;
+	char *s1 = "aAeEoOtTlL";
+	char *s2 = "4433007711";
 
 	i = 0;
 	while (s[i] != '\0')
-		i++;
-
-	for (j = 0; j < i; j++)
 	{
-		if (s[j] == 'a' || s[j] == 'A')
-			s[j] = '4';
-		else if (s[j] == 'e' || s[j] == 'E')
-			s[j] = '3';
-		else if (s[j] == 'o' || s[j] == 'O')
-			s[j] = '0';
-		else if (s[j] == 't' || s[j] == 'T')
-			s[j] = '7';
-		else if (s[j] == 'l' || s[j] == 'L')
-			s[j] = '1';
+		j = 0;
+		while (j < 10)
+		{
+			if (s[i] == s1[j])
+				s[i] = s2[j];
+			j++;
+		}
+		i++;
 	}
 	return (s);
 }
