bcf892464210a4e6c572395ced8d5da6e1dbede7
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-26 18:57:26 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strchr - return first occurence of char
+ * @s: str
+ * @c: chr
+ * Return: chr * or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i, j, k;
+	char ar[100];
+	char *ppt;
+
+	j = 0;
+	while (s[j] != '\0')
+		j++;
+
+	i = 0;
+	while ((s[i] != c) && (i < j))
+		i++;
+
+	if (i != j)
+	{
+		for (k = i; k < j; k++)
+			ar[k - i] = s[k];
+		ppt = ar;
+	}
+	return ppt;
+}
