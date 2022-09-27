bd2bb3ace32f018436b89145fdfd73dd883fa537
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-27 06:32:11 -0700

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,29 +2,23 @@
 #include <stdio.h>
 
 /**
- * *_strpbrk - search str for set of bytes
+ * *_strpbrk - find str
  * @s: str
  * @accept: substr
  * Return: char * or NULL
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int a, b, i, j;
-	char *p = NULL;
+	int i, j;
 
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (accept[j] == s[i])
-				a = i;
-
-			if (!b || a < b)
-				b = a;
+				return (s);
 		}
+		s++;
 	}
-	if (b)
-		p = (s + b);
-	return (p);
+	return (NULL);
 }
