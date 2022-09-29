e9a9eaaf0a8d8f9441bbd26614df90b92d71b916
Aghogho Monorien <monorienaghogho@gmail.com>
2022-09-29 07:24:17 -0700

    2. strlen recursively

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - get strlen recursively
+ * @s: str
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+	return 0;
+}
