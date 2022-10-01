f07cc16685067d0a1504b6c80cc95baebbeae60e
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-01 09:09:29 -0700

    3. mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: num of args
+ * @argv: args vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 1;
+
+	if (argc > 1)
+	{
+		while (argv[i])
+		{
+			sum *= atoi(argv[i++]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
