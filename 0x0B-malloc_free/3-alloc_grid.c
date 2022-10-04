af03374b0c51879afd13c1c9acb6be45b3fd048b
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-04 09:33:57 -0700

    3. alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = malloc(sizeof(int) * height);
+	p = malloc(sizeof(int *) * height);
 
 	if (p == NULL)
 		return (NULL);
