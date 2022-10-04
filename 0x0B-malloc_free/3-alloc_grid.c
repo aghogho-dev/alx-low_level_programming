2c887f3262bf78cb4d0d3fa64c0d0e6022de3abf
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-04 09:26:53 -0700

    3. alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,9 +34,10 @@ int **alloc_grid(int width, int height)
 				free(p[j]);
 			return (NULL);
 		}
-
+	}
+	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			p[i][j] = 0;
-	}
+
 	return (p);
 }
