f725608f132e1e10534b0864b8c3b061cb85bcb2
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-10 16:59:22 -0700

    0. struct poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - describe dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
