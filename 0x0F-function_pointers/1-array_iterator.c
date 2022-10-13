5c38159fa685c7e6ecf52ceca6b3fe7b5b6863f0
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-12 18:14:31 -0700

    1. array iter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - func
+ * @array: param
+ * @size: param
+ * @action: param
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
