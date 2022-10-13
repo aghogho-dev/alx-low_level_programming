06932849ba25383041a439b18c82a903c3453a3a
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-12 20:28:44 -0700

    function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check code
+ * @argc: param
+ * @argv: param
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i, j;
+	char *s;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	i = atoi(argv[1]);
+	if (!i)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	s = (char *)main;
+
+	for (j == 0; j < i; i++)
+	{
+		if (j == i - 1)
+		{
+			printf("%02x\n", s[i]);
+			break;
+		}
+		printf("%02x ", s[i]);
+	}
+	return (0);
+}
