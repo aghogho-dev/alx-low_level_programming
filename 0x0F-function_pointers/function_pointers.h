aa55e8c95e41a7db4a6828fd9dd7ca3ddafd84c8
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-12 18:19:05 -0700

    1. array iter

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,10 @@
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * size_t - equlas unsigned long int
+ */
+typedef unsigned long int size_t;
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
