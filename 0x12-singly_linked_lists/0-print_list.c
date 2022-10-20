afa6e5df0649af3e9140c422df07329f9de04e62
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-20 09:14:15 -0700

    0. print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - func
+ * @h: param
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
