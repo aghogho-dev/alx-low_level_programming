ad8e0ac8e44d7c7fd8ce33098c297f13e06eddca
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-27 21:49:09 -0700

    2. get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned log int) * 8))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	if ((n & (1 << index)) == 0)
