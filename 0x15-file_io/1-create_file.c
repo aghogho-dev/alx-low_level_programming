f0569b7f4ca60cf6b8779aa923ce10bb6a2fe14d
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-31 14:56:28 -0700

    2. create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,6 +26,8 @@ int create_file(const char *filename, char *text_content)
 	if (op == -1 || wr == -1)
 		return (-1);
 
-	close(op);
+	k = close(op);
+	if (k == -1)
+		return (-1);
 	return (1);
 }
