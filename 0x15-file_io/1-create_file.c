ec6f52cf661a85cb6744d8b04e77afe0166d93ef
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-31 14:46:11 -0700

    2. create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - func
+ * @filename: param
+ * @text_content: param
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr, i, k = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+		{
+			k++;
+		}
+	}
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, i);
+
+	if (op < 0 || wr < 0)
+		return (-1);
+
+	close(op);
+	return (1);
+}
