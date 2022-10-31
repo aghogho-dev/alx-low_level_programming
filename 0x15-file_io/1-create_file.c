5b6f6fd020e3f6abf82f1b1d4a75bedc8ce2dacd
Aghogho Monorien <monorienaghogho@gmail.com>
2022-10-31 14:53:49 -0700

    2. create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			k++;
 		}
 	}
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	wr = write(op, text_content, i);
 
 	if (op == -1 || wr == -1)
